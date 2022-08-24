9bdcc917a25297ece45d80a8f80e246471879975
project-ijon <chrisjhonijon@gmail.com>
2022-08-24 10:48:14 +0800

    Home responsive

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,53 +1,60 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Alert } from "react-bootstrap";
 import { faCircleExclamation, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Home = () => {
   return (
     <React.Fragment>
-      <Container style={{ marginTop: "100px", color: "black" }}>
-        <div
-          style={{
-            backgroundColor: "#5F9EA0",
-            fontSize: "30px",
+      <>
+        <Container>
+          <div
+            style={{
+              marginTop: "100px",
+              backgroundColor: "#5F9EA0",
+              fontSize: "30px",
+              width: "100%",
+              padding: "20px",
+              boxShadow: "20PX 25PX 10PX -5px rgba(0, 0, 0, 0.8)",
+              color: "black",
+            }}
+          >
+            <h2>
+              <u>
+                <b>Tugas Dea Cource</b>
+              </u>
+            </h2>
+            <br />
 
-            width: "100%",
-            height: "500px",
-            marginTop: "30px",
-            padding: "20px",
-            boxShadow: "20PX 25PX 10PX -5px rgba(0, 0, 0, 0.8)",
-          }}
-        >
-          <p style={{ fontWeight: "bold", textDecoration: "underline" }}>Tugas Dea Cource</p>
-
-          <p>
-            1. Routing URL post dengan isian api POST <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />{" "}
-          </p>
-          <p>
-            {" "}
-            2. Modifikasi UI Homepage <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />
-          </p>
-          <p>
-            {" "}
-            3. Membuat Active Navigation Bar <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />{" "}
-          </p>
-          <p>
-            {" "}
-            4. Membuat modal popup dari API POST <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />
-          </p>
-          <p>
-            {" "}
-            5. Membuat UI Kreatifitas Masing-Masing <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />
-          </p>
-          <br />
-          <p>
-            Sumber API :{" "}
-            <a href="https://jsonplaceholder.typicode.com/photos" target="_blank">
-              https://jsonplaceholder.typicode.com
-            </a>
-          </p>
-        </div>
-      </Container>
+            <p>
+              1. Routing URL post dengan isian api POST <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />{" "}
+            </p>
+            <p>
+              {" "}
+              2. Modifikasi UI Homepage <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />
+            </p>
+            <p>
+              {" "}
+              3. Membuat Active Navigation Bar <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />{" "}
+            </p>
+            <p>
+              {" "}
+              4. Membuat modal popup dari API POST <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />
+            </p>
+            <p>
+              {" "}
+              5. Membuat UI Kreatifitas Masing-Masing <FontAwesomeIcon icon={faCheck} className="ms-2 text-success" />
+            </p>
+            <br />
+            <p>
+              Sumber API :{" "}
+              <a href="https://jsonplaceholder.typicode.com/photos" target="_blank">
+                https://jsonplaceholder.typicode.com
+              </a>
+            </p>
+          </div>
+        </Container>
+      </>
     </React.Fragment>
   );
 };
--- src/components/Layout/navigation.layout.js
+++ src/components/Layout/navigation.layout.js
@@ -5,45 +5,48 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark" fixed="top">
+      <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
         <Container>
-          <Navbar.Brand></Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink
-              to="/"
-              style={({ isActive }) => ({
-                color: isActive ? "#fff" : "#708090",
-                textDecoration: isActive ? "none" : "none",
-                borderBottom: isActive ? "thin solid #166991" : "none",
-              })}
-              className="me-3"
-            >
-              <h6>Home</h6>
-            </NavLink>
-            <NavLink
-              to="/albums"
-              style={({ isActive }) => ({
-                color: isActive ? "#fff" : "#708090",
-                textDecoration: isActive ? "none" : "none",
-                borderBottom: isActive ? "thin solid #166991" : "none",
-              })}
-              className="me-3"
-            >
-              <h6>Albums</h6>
-            </NavLink>
+          <Navbar.Brand href="/">React JS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink
+                to="/"
+                style={({ isActive }) => ({
+                  color: isActive ? "#fff" : "#708090",
+                  textDecoration: isActive ? "none" : "none",
+                  borderBottom: isActive ? "thin solid #166991" : "none",
+                })}
+                className="me-3"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/albums"
+                style={({ isActive }) => ({
+                  color: isActive ? "#fff" : "#708090",
+                  textDecoration: isActive ? "none" : "none",
+                  borderBottom: isActive ? "thin solid #166991" : "none",
+                })}
+                className="me-3"
+              >
+                Albums
+              </NavLink>
 
-            <NavLink
-              to="/posts"
-              style={({ isActive }) => ({
-                color: isActive ? "#fff" : "#708090",
-                textDecoration: isActive ? "none" : "none",
-                borderBottom: isActive ? "thin solid #166991" : "none",
-              })}
-              className="me-3"
-            >
-              <h6> Post </h6>
-            </NavLink>
-          </Nav>
+              <NavLink
+                to="/posts"
+                style={({ isActive }) => ({
+                  color: isActive ? "#fff" : "#708090",
+                  textDecoration: isActive ? "none" : "none",
+                  borderBottom: isActive ? "thin solid #166991" : "none",
+                })}
+                className="me-3"
+              >
+                Post
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
